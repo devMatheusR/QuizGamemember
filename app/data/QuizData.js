bb59fb9df25b2595d37586d1a22b03b14546a9fc
devMatheusR <devmatheusr@gmail.com>
2021-11-08 22:09:18 -0300

    Push Geral

---
+++ app/data/QuizData.js
@@ -0,0 +1,27 @@
+export default data = [
+    {
+        question: "Em qual cidade você nasceu? ",
+        options: ["Salvador","Camaçari","Recife","Manaus"],
+        correct_option: "Camaçari"
+    },
+    {
+        question: "Você praticava esportes na adolescência? Se sim, qual?",
+        options: ["Não","Sim, Natação","Sim, Vôlei","Sim, Atletismo"],
+        correct_option: "Sim, Vôlei"
+    },
+    {
+        question: " Qual o nome da sua mãe? ",
+        options: ["Luiza","Joice","Daniela","Maria"],
+        correct_option: "Daniela"
+    },
+    {
+        question: "Quantos irmãos você tem? ",
+        options: ["0","2","1","4"],
+        correct_option: "1"
+    },
+    {
+        question: "Você tem cônjuge? ",
+        options: ["Não", "Sim"],
+        correct_option: "Não"
+    }
+]
